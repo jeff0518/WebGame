e959624776f204300c0853049a6d10fb4624380c
jeff0518 <cycivs07@icloud.com>
2023-11-03 11:29:24 +0800

    feat: add 卡片翻轉的css

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -5,9 +5,20 @@ function Main() {
     <>
       <div className={style.main_container}>
         <div className={style.main_container_section}>
-          <div className={style.section_card}>卡片1</div>
-          <div className={style.section_card}>卡片2</div>
-          <div className={style.section_card}>卡片3</div>
+          <div className={style.cardInbox}>
+            <div
+              className={`${style.cardInbox__side} ${style.cardInbox__side__front}`}
+            >
+              卡片Front
+            </div>
+            <div
+              className={`${style.cardInbox__side} ${style.cardInbox__side__back}`}
+            >
+              卡片Back
+            </div>
+          </div>
+          <div className={style.cardInbox}>卡片2</div>
+          <div className={style.cardInbox}>卡片3</div>
         </div>
       </div>
     </>
