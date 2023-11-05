04423ad03105da832a41d2752094a65095cebd0b
jeff0518 <cycivs07@icloud.com>
2023-11-05 10:41:33 +0800

    fix: card style

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -9,16 +9,22 @@ function Main() {
           <Card
             color="cardInbox__side__back__color1"
             card_image="front__img__1"
+            title="井字遊戲"
+            description="井字遊戲你不會玩？"
             gameId="tictactoe"
           />
           <Card
             color="cardInbox__side__back__color2"
             card_image="front__img__2"
+            title="貪食蛇"
+            description="貪食蛇你懂的"
             gameId="snake"
           />
           <Card
             color="cardInbox__side__back__color3"
             card_image="front__img__3"
+            title="翻牌遊戲"
+            description="建構中"
             gameId="123"
           />
         </div>
--- src/components/main/card/Card.tsx
+++ src/components/main/card/Card.tsx
@@ -5,28 +5,27 @@ import style from "./Card.module.scss";
 interface CardProps {
   color: string;
   card_image: string;
+  title: string;
+  description: string;
   gameId: string;
 }
 
-function Card({ color, card_image, gameId }: CardProps) {
+function Card({ color, card_image, gameId, description, title }: CardProps) {
   return (
     <div className={style.cardInbox}>
       <div
         className={`${style.cardInbox__side} ${style.cardInbox__side__front}`}
       >
         <div className={`${style.front__img} ${style[card_image]}`} />
-        <div className={style.front__title}>title 30%</div>
+        <div className={style.front__title}>{title}</div>
       </div>
       <div
         className={`${style.cardInbox__side} ${style.cardInbox__side__back} ${style[color]}`}
       >
         <div className={style.back__container}>
-          <div className={style.description}>
-            描述遊戲玩法 70%。如果你想要用英文表达 "新增卡片前的格式"，可以使用
-            "Formatting for Adding Ca
-          </div>
+          <div className={style.description}>{description}</div>
           <NavLink to={`/games/${gameId}`}>
-            <button className={style.card__button}>button 的連結 30%</button>
+            <button className={style.card__button}>Play</button>
           </NavLink>
         </div>
       </div>
